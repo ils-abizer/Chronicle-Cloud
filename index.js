e6944db06cb6062cf584a9af88604558bbcfb9b5
ils-abizer <a.lohawala@thesynapses.com>
2023-04-05 17:56:27 +0530

    validation and forgot page created

--- index.js
+++ index.js
@@ -5,19 +5,24 @@ function seterror(id,error){
 
 }
 
-function remove_error(id,error) {
-    element=document.getElementById(id)
-    element.getElementsByClassName
+// function remove_error(id,error) {
+//     element=document.getElementById(id)
+//     element.getElementsByClassName
 
-}
+// }
 function validate(){
-    console.log("validating")
+    console.log("validating");
     var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     var flag=true;
     var name=document.forms['myform']["email_Id"].value;
-    if(! name.match(validRegex)){
-        seterror("input_ID","*Please enter valid email address");
+    if(! name.match(validRegex))
+    {
+        seterror("form_id","*Please enter valid email address");
             flag=false;
+    }
+    if(name.match(validRegex))
+    {
+        seterror('form_id',"");
     }
     return flag;
 
