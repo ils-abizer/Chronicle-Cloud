c4b864e5167d9293e93de8fa8c13735ba50a1464
ils-abizer <a.lohawala@thesynapses.com>
2023-04-05 13:51:39 +0530

    Login_Register Changed

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-// const validate {
-//     "email" :
-// }
+function seterror(id,error){
+    element=document.getElementById(id);
+    element.getElementsByClassName('errormsg')[0].innerHTML=error;
+
+
+}
+
+function remove_error(id,error) {
+    element=document.getElementById(id)
+    element.getElementsByClassName
+
+}
+function validate(){
+    console.log("validating")
+    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var flag=true;
+    var name=document.forms['myform']["email_Id"].value;
+    if(! name.match(validRegex)){
+        seterror("input_ID","*Please enter valid email address");
+            flag=false;
+    }
+    return flag;
+
+}
